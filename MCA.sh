9cfd5ee9a407a7501e058185ab22a4a5fb9dbbf7
taufiqx404 <59577411+taufiqx404@users.noreply.github.com>
2020-09-05 21:45:25 +0800

    Update and rename README.md to MCA.sh

---
+++ MCA.sh
@@ -0,0 +1,199 @@
+# MCA-TOOLS
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+#batas anjng
+echo $me"╔═╗╔═╗╔╗─╔╗╔═══╗╔╗───╔══╗╔═╗╔═╗"
+echo $me"║║╚╝║║║║─║║║╔═╗║║║───╚╣─╝║║╚╝║║"
+echo $me"║╔╗╔╗║║║─║║║╚══╗║║────║║─║╔╗╔╗║"
+echo $pu"║║║║║║║║─║║╚══╗║║║─╔╗─║║─║║║║║║"
+echo $pu"║║║║║║║╚═╝║║╚═╝║║╚═╝║╔╣─╗║║║║║║"
+echo $pu"╚╝╚╝╚╝╚═══╝╚═══╝╚═══╝╚══╝╚╝╚╝╚╝"
+echo
+sleep 3
+echo $cy"╔═══╗╔╗──╔╗╔══╗─╔═══╗╔═══╗"
+echo $cy"║╔═╗║║╚╗╔╝║║╔╗║─║╔══╝║╔═╗║"
+echo $cy"║║─╚╝╚╗╚╝╔╝║╚╝╚╗║╚══╗║╚═╝║"
+echo $pr"║║─╔╗─╚╗╔╝─║╔═╗║║╔══╝║╔╗╔╝"
+echo $pr"║╚═╝║──║║──║╚═╝║║╚══╗║║║╚╗"
+echo $pr"╚═══╝──╚╝──╚═══╝╚═══╝╚╝╚═╝"
+echo
+sleep 3
+echo $ij"╔═══╗╔═══╗╔═╗╔═╗╔╗──╔╗"
+echo $ij"║╔═╗║║╔═╗║║║╚╝║║║╚╗╔╝║"
+echo $ij"║║─║║║╚═╝║║╔╗╔╗║╚╗╚╝╔╝"
+echo $bi"║╚═╝║║╔╗╔╝║║║║║║─╚╗╔╝─"
+echo $bi"║╔═╗║║║║╚╗║║║║║║──║║──"
+echo $bi"╚╝─╚╝╚╝╚═╝╚╝╚╝╚╝──╚╝──"
+sleep 3
+clear
+echo $ku" ASSALAMUALAIKUM SAHABAT CYBER "
+sleep 1
+echo $ku" SEMOGA SEHAT SELALU DAN DI BAWAH PERLINDUNGAN ALLAH SWT"
+clear
+echo $me" TOOLS INI BERISI TENTANG AYAT AYAT SUCI ALQURAN"
+sleep 1
+echo $ij" SIAPA SAJA YANG MEMBACA NYA MUDAH-MUDAHAN DI BERI SAFAAT OLEH ALLAH SWT"
+sleep 1
+clear
+read -p " SIAPA NAMA ANDA?=" pill
+sleep 2
+echo $ku" SELAMAT MEMBACA KAWAN "
+clear
+echo $ij"________PILIH AYAT___________"
+echo $ku"1."$pr" AYAT KURSI     |  M |"
+echo $me"2."$ij" AL-FATIHAH     |  U |"
+echo $bi"3."$me" AL-BAQARAH     |  S |"
+echo $ku"4."$ku" AL-IMRAN       |  L |"
+echo $bi"5."$bi" AL-MAIDAH      |  I |"
+echo $cy"6."$cy" AN-NISA        |  M |"
+echo $ij"7."$ij" AT-TAUBAH     | CYBER |"
+echo $bi"8."$bi" AN-NUR         |  A |"
+echo $me"9."$me"  YASIN         |  R |"
+echo $cy"10."$cy" AL-SAFAAT     |  M |"
+echo $ku"11."$ku" AL-KAHF       |  Y |"
+echo $in"_____________________________"
+echo $ku"    ASSALAMUALAIKUM "
+read -p"MUSLIM CYBER ARMY___♦•>" pil
+
+if [ $pil = 1 ]
+then
+clear
+echo $ku"ۚ اللّٰـهُ لَآ إِلٰهَ إِلَّا هُوَ الْحَىُّ الْقَيُّوم"ُ
+echo
+echo $ku"Allah tidak ada Tuhan (yang berhak disembah) melainkan Dia yang Maha Kekal lagi terus menerus makhlukNya"
+echo
+echo  $ku" لا تَأْخُذُهُۥ سِنَةٌ وَلَا نَوْم"ٌ
+echo
+echo $ku"Tidak mengantuk dan tidak tidur"
+echo
+echo $ku" ۗ لَّهُۥ مَا فِى السَّمٰوٰتِ وَمَا فِى الْأَرْض"ِ
+echo
+echo $ku"MilikNya lah apa yang di langit dan di bumi"
+echo
+echo $ku"ۚ مَن ذَا الَّذِى يَشْفَعُ عِندَهُۥٓ إِلَّا بِإِذْنِهِ ۦ"
+echo
+echo $ku"Siapakah yang dapat memberi syafa'at di sisi Allah tanpa izinNya?"
+echo
+echo $ku"ۖ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ"
+echo
+echo $ku"Allah melihat apa-apa yang di hadapan mereka dan di belakang meraka"
+echo
+echo $ku" ۚ وَلَا يُحِيطُونَ بِشَىْءٍ مِّنْ عِلْمِهِۦٓ إِلَّا بِمَا شَآء"
+echo
+echo $ku"Dan mereka tidak melihat apa-apa dari ilmu Allah melainkan apa yang dikehendakiNya"
+echo
+echo $ku"  ۖ وَسِعَ كُرْسِيُّهُ السَّمٰوٰتِ وَالْأَرْضَ"
+echo
+echo $ku"Kursi Allah termasuk langit dan bumi"
+echo
+echo $ku"وَلَا يَـُٔودُهُۥ حِفْظُهُمَا ۚ وَهُوَ الْعَلِىُّ الْعَظِيمُ"
+echo
+echo $ku"Dan Allah tidak merasa berat berkendara, dan Allah Maha Tinggi lagi Maha Besar"
+echo
+echo $ku"Ayat Kursi QS Al-Baqarah 255"
+echo
+echo $me" MUSLIM CYBER ARMY"
+fi
+
+if [ $pil = 2 ]
+then
+clear
+echo $pr"بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْم"ِ
+echo $pr"Dengan nama Allah Yang Maha Pengasih, Maha Penyayang"
+echo
+echo $pr"اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِيْنَ"
+echo $pr"Segala puji bagi Allah, Tuhan seluruh alam,"
+echo
+echo $pr"الرَّحْمٰنِ الرَّحِيْمِ"
+echo $pr"Yang Maha Pengasih, Maha Penyayang,"
+echo
+echo $pr"مٰلِكِ يَوْمِ الدِّيْنِ"
+echo $pr"Pemilik hari pembalasan."
+echo
+echo $pr"اِيَّاكَ نَعْبُدُ وَاِيَّاكَ نَسْتَعِيْنُ"
+echo $pr"Hanya kepada Engkaulah kami menyembah dan hanya kepada Engkaulah kami mohon pertolongan."
+echo
+echo $pr"اِھْدِنَا الصِّرَاطَ الْمُسْتَـقِيْمَ"
+echo $pr"Tunjukilah kami jalan yang lurus"
+echo
+echo $pr"صِرَاطَ الَّذِيۡنَ اَنۡعَمۡتَ عَلَيۡهِمۡ ۙ غَيۡرِ الۡمَغۡضُوۡبِ عَلَيۡهِمۡ وَلَا الضَّآلِّيۡنَ"
+echo $pr"jalan orang-orang yang telah Engkau beri nikmat kepadanya; bukan (jalan) mereka yang dimurkai, dan bukan (pu                       >
+echo
+echo $me" MUSLIM CYBER ARMY "
+fi
+
+if [ $pil = 3 ]
+then
+clear                                                                                                                                        echo $cy"بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْم"
+echo $cy"الٓمّٓۚ"
+echo $cy"Alif Lam Mim"
+echo
+echo $cy"ذٰلِكَ الْكِتٰبُ لَا رَيْبَ ۛ فِيْهِ ۛ ھُدًى لِّلْمُتَّقِيْنَ"
+echo $cy"Kitab (Al-Qur'an) ini tidak ada keraguan padanya; petunjuk bagi mereka yang bertakwa,"
+echo
+echo $cy"الَّذِيْنَ يُؤْمِنُوْنَ بِالْغَيْبِ وَ يُـقِيْمُوْنَ الصَّلٰوةَ وَ مِمَّا رَزَقْنٰھُمْ يُنْفِقُوْنَ"
+echo $cy"(yaitu) mereka yang beriman kepada yang gaib, melaksanakan shalat, dan menginfakkan sebagian rezeki yang Kam                       >
+echo
+echo $cy"وَ الَّذِيْنَ يُؤْمِنُوْنَ بِمَآ اُنْزِلَ اِلَيْكَ وَمَآ اُنْزِلَ مِنْ قَبْلِكَ ۚ وَبِالْاٰخِرَةِ ھُمْ يُوْقِنُوْنَ"
+echo $cy"dan mereka yang beriman kepada (Al-Qur'an) yang diturunkan kepadamu (Muhammad) dan (kitab-kitab) yang telah                        >
+echo
+echo $cy"اُولٰٓٮِٕكَ عَلٰى هُدًى مِّنۡ رَّبِّهِمۡ‌ وَاُولٰٓٮِٕكَ هُمُ الۡمُفۡلِحُوۡنَ‏"
+echo $cy"Merekalah yang mendapat petunjuk dari Tuhannya, dan mereka itulah orang-orang yang beruntung."
+echo
+echo $cy"اِنَّ الَّذِيۡنَ كَفَرُوۡا سَوَآءٌ عَلَيۡهِمۡ ءَاَنۡذَرۡتَهُمۡ اَمۡ لَمۡ تُنۡذِرۡهُمۡ لَا يُؤۡمِنُوۡنَ‏"                                                                          echo $cy"Sesungguhnya orang-orang kafir, sama saja bagi mereka, engkau (Muhammad) beri peringatan atau tidak engkau b                       >
+echo
+echo $cy"خَتَمَ اللّٰهُ عَلَىٰ قُلُوۡبِهِمۡ وَعَلٰى سَمۡعِهِمۡ‌ؕ وَعَلٰىٓ اَبۡصَارِهِمۡ غِشَاوَةٌ وَّلَهُمۡ عَذَابٌ عَظِيۡمٌ"
+echo $cy"Allah telah mengunci hati dan pendengaran mereka, penglihatan mereka telah tertutup, dan mereka akan mendapa                       >
+echo
+echo $cy"وَمِنَ النَّاسِ مَنۡ يَّقُوۡلُ اٰمَنَّا بِاللّٰهِ وَبِالۡيَوۡمِ الۡاٰخِرِ وَمَا هُمۡ بِمُؤۡمِنِيۡنَ‌ۘ‏"
+echo $cy"Dan di antara manusia ada yang berkata, Kami beriman kepada Allah dan hari akhir, padahal sesungguhnya merek                       >echo
+echo $cy"يُخٰدِعُوۡنَ اللّٰهَ وَالَّذِيۡنَ اٰمَنُوۡا ‌ۚ وَمَا يَخۡدَعُوۡنَ اِلَّاۤ اَنۡفُسَهُمۡ وَمَا يَشۡعُرُوۡنَؕ"
+echo $cy"Mereka menipu Allah dan orang-orang yang beriman, padahal mereka hanyalah menipu diri sendiri tanpa mereka s                       >echo
+echo $cy"فِىۡ قُلُوۡبِهِمۡ مَّرَضٌۙ فَزَادَهُمُ اللّٰهُ مَرَضًا ‌ۚ وَّلَهُمۡ عَذَابٌ اَلِيۡمٌۙۢ بِمَا كَانُوۡا يَكۡذِبُوۡنَ‏"
+echo $cy"Dalam hati mereka ada penyakit, lalu Allah menambah penyakitnya itu; dan mereka mendapat azab yang pedih, ka                       >
+echo
+echo $cy"وَاِذَا قِيۡلَ لَهُمۡ لَا تُفۡسِدُوۡا فِىۡ الۡاَرۡضِۙ قَالُوۡاۤ اِنَّمَا نَحۡنُ مُصۡلِحُوۡنَ"
+echo $cy"Dan apabila dikatakan kepada mereka, Janganlah berbuat kerusakan di bumi! Mereka menjawab, Sesungguhnya kami                       >
+echo
+echo $cy"اَلَا ۤ اِنَّهُمۡ هُمُ الۡمُفۡسِدُوۡنَ وَلٰـكِنۡ لَّا يَشۡعُرُوۡنَ"                                                                                              echo $cy"Ingatlah, sesungguhnya merekalah yang berbuat kerusakan, tetapi mereka tidak menyadari."
+echo                                                                                                                                         echo $cy"وَاِذَا قِيۡلَ لَهُمۡ اٰمِنُوۡا كَمَاۤ اٰمَنَ النَّاسُ قَالُوۡاۤ اَنُؤۡمِنُ كَمَاۤ اٰمَنَ السُّفَهَآءُ‌ ؕ اَلَاۤ اِنَّهُمۡ هُمُ السُّفَهَآءُ وَلٰـكِنۡ لَّا يَعۡلَمُوۡنَ"
+echo $cy"Dan apabila dikatakan kepada mereka, Berimanlah kamu sebagaimana orang lain telah beriman! Mereka menjawab,                        >
+echo
+echo $cy"وَاِذَا لَقُوۡا الَّذِيۡنَ اٰمَنُوۡا قَالُوۡاۤ اٰمَنَّا ۖۚ وَاِذَا خَلَوۡا اِلٰى شَيٰطِيۡنِهِمۡۙ قَالُوۡاۤ اِنَّا مَعَكُمۡۙ اِنَّمَا نَحۡنُ مُسۡتَهۡزِءُوۡنَ"
+echo $cy"Dan apabila mereka berjumpa dengan orang yang beriman, mereka berkata, "Kami telah beriman." Tetapi apabila                        >
+echo
+echo $cy"اَللّٰهُ يَسۡتَهۡزِئُ بِهِمۡ وَيَمُدُّهُمۡ فِىۡ طُغۡيَانِهِمۡ يَعۡمَهُوۡنَ‏"
+echo $cy"Allah akan memperolok-olokkan mereka dan membiarkan mereka terombang-ambing dalam kesesatan."
+echo                                                                                                                                         echo $cy"اُولٰٓٮِٕكَ الَّذِيۡنَ اشۡتَرَوُا الضَّلٰلَةَ بِالۡهُدٰى فَمَا رَبِحَتۡ تِّجَارَتُهُمۡ وَمَا كَانُوۡا مُهۡتَدِيۡنَ‏"
+echo $cy"Mereka itulah yang membeli kesesatan dengan petunjuk. Maka perdagangan mereka itu tidak beruntung dan mereka                       >
+echo
+echo
+echo $cy"مَثَلُهُمۡ كَمَثَلِ الَّذِى اسۡتَوۡقَدَ نَارًا ‌ۚ فَلَمَّاۤ اَضَآءَتۡ مَا حَوۡلَهٗ ذَهَبَ اللّٰهُ بِنُوۡرِهِمۡ وَتَرَكَهُمۡ فِىۡ ظُلُمٰتٍ لَّا يُبۡصِرُوۡنَ"
+echo $cy"Perumpamaan mereka seperti orang-orang yang menyalakan api, setelah menerangi sekelilingnya, Allah melenyapkan cahaya (yang menyina>
+echo
+echo $cy"صُمٌّۢ بُكۡمٌ عُمۡىٌ فَهُمۡ لَا يَرۡجِعُوۡنَ ۙ‏"
+echo $cy"Mereka tuli, bisu dan buta, sehingga mereka tidak dapat kembali."
+echo
+echo $cy"اَوۡ كَصَيِّبٍ مِّنَ السَّمَآءِ فِيۡهِ ظُلُمٰتٌ وَّرَعۡدٌ وَّبَرۡقٌ‌ ۚ يَجۡعَلُوۡنَ اَصَابِعَهُمۡ فِىۡۤ اٰذَانِهِمۡ مِّنَ الصَّوَاعِقِ حَذَرَ الۡمَوۡتِ‌ؕ وَاللّٰهُ مُحِيۡطٌ‌ۢ بِالۡكٰفِرِيۡنَ"
+echo $cy"Atau seperti (orang yang ditimpa) hujan lebat dari langit, yang disertai kegelapan, petir dan kilat. Mereka menyumbat telinga denga>
+echo
+echo $cy"يَكَادُ الۡبَرۡقُ يَخۡطَفُ اَبۡصَارَهُمۡ‌ؕ كُلَّمَاۤ اَضَآءَ لَهُمۡ مَّشَوۡا فِيۡهِ وَاِذَاۤ اَظۡلَمَ عَلَيۡهِمۡ قَامُوۡا‌ؕ وَلَوۡ شَآءَ اللّٰهُ لَذَهَبَ بِسَمۡعِهِمۡ وَاَبۡصَارِهِمۡ‌ؕ اِنَّ اللّٰهَ عَلٰى كُلِّ شَىۡءٍ قَدِيۡرٌ"
+echo $cy"Hampir saja kilat itu menyambar penglihatan mereka. Setiap kali (kilat itu) menyinari, mereka berjalan di bawah (sinar) itu, dan ap>
+echo
+echo $me" MUSLIM CYBER ARMY"
+fi
+
+if [ $pil = 5 ]
+then
+clear
+echo $ku" BUAT SC 5 SAMPAI 11 LAGI DI PERBAIKI MOHON MAAF ATAS KESALAHAN INI TERIMAKASIH"
+sleep 1
+echo $ku" MUSLIM CYBER ARMY"
+fi
